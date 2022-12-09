0b49b3118399aaab0a05e37ae62fa6b0a888760c
souradeep98 <souradeepmondal414@gmail.com>
2022-12-10 01:54:03 +0530

    nothing

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    NotificationListenerHelper.stopListening();
+    //NotificationListenerHelper.stopListening();
   }
 
   @override
